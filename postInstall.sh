6f2dab64244eb79b78df79b7be33632c972ce957
sakresh <sarkeshnandu@gmail.com>
2023-09-13 22:51:32 +0530

    Arch Post-Install script(Personal use)

---
+++ postInstall.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+echo "Starting the Script"
+
+pacman -S git wget curl neovim ttf-material-design-icons-git ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols ttf-font-awesome ttf-unifont ttf-hack-nerd ttf-jetbrains-mono-nerd ttf-unifont ttf-noto-nerd noto-fonts noto-fonts-emoji bspwm sxhkd polybar xorg xorg-xinit picom nitrogen blueman pipewire telegram-desktop thunar rofi unzip zsh yt-dlp npm nodejs
+
+#mkdir -p ~/.config && git clone https://github.com/sakresh/dotFiles && cd dotFiles && cp -r * ~/.config/
+
